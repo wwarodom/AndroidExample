904f72d7722f0f32a601452eed49bfd2e802eed6
Warodom Werapun <wwarodom@gmail.com>
2017-06-20 22:42:54 +0700

    Fragment example (with errors)

---
+++ FragmentExample/app/src/main/java/com/warodom/fragmentexample/MainActivity.java
@@ -0,0 +1,17 @@
+package com.warodom.fragmentexample;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.fragment_order_list);
+        OrderListFragment orderListFragment = (OrderListFragment) fragment;
+    }
+}
